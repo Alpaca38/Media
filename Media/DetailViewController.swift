e4a7cc6d60849e8aad79357c321d61e4159d4ae8
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-06-22 16:54:22 +0900

    상세화면 뷰 분리

--- Media/DetailViewController.swift
+++ Media/DetailViewController.swift
@@ -12,128 +12,49 @@ import Toast
 
 class DetailViewController: BaseViewController {
     
-    let posterImageView = UIImageView()
-    let infoView = UIView()
-    let titleLabel = UILabel()
-    let overviewLabel = UILabel()
-    let castLabel = UILabel()
-    let castTableView = UITableView()
-    
+    let detailView = DetailView()
     var data: movieResult?
     
-    var list: [Cast] = [] {
-        didSet {
-            castTableView.reloadData()
-        }
+    override func loadView() {
+        super.loadView()
+        view = detailView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getMovieCredit()
-        configureHierachy()
-        configureLayout()
+        detailView.getMovieCredit(data: data!)
         configureUI()
     }
 
 }
 
-extension DetailViewController: ConfigureProtocol {
-    func configureHierachy() {
-        view.addSubviews([posterImageView, infoView])
-        infoView.addSubviews([titleLabel, overviewLabel, castLabel, castTableView])
-    }
-    
-    func configureLayout() {
-        posterImageView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        infoView.snp.makeConstraints {
-            $0.top.equalTo(view.snp.centerY)
-            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
-        }
-        
-        titleLabel.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(30)
-            $0.top.equalToSuperview().offset(20)
-        }
-        
-        overviewLabel.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(30)
-            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
-        }
-        
-        castLabel.snp.makeConstraints {
-            $0.top.equalTo(overviewLabel.snp.bottom).offset(20)
-            $0.leading.equalTo(overviewLabel)
-        }
-        
-        castTableView.snp.makeConstraints {
-            $0.top.equalTo(castLabel.snp.bottom).offset(8)
-            $0.horizontalEdges.equalTo(overviewLabel)
-            $0.bottom.equalToSuperview()
-        }
-        
-    }
-    
+extension DetailViewController {
     func configureUI() {
         setNavigationBar(tintColor: .black, title: nil)
         
-        castTableView.delegate = self
-        castTableView.dataSource = self
-        castTableView.register(CastTableViewCell.self, forCellReuseIdentifier: CastTableViewCell.identifier)
-        castTableView.rowHeight = 80
+        detailView.castTableView.delegate = self
+        detailView.castTableView.dataSource = self
         
-        posterImageView.contentMode = .scaleAspectFill
         let url = URL(string: "https://image.tmdb.org/t/p/original\(data!.posterPath)")
-        posterImageView.kf.setImage(with: url)
+        detailView.posterImageView.kf.setImage(with: url)
         
-        infoView.backgroundColor = .white
-        infoView.clipsToBounds = true
-        infoView.layer.cornerRadius = 20
-        infoView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        detailView.titleLabel.text = data?.title
         
-        titleLabel.font = .bigTitleFont
-        titleLabel.textColor = .defaultColor
-        titleLabel.numberOfLines = 0
-        titleLabel.textAlignment = .center
-        titleLabel.text = data?.title
-        
-        overviewLabel.font = .contentFont
-        overviewLabel.textColor = .defaultColor
-        overviewLabel.numberOfLines = 0
-        overviewLabel.text = data?.overview
-        
-        castLabel.font = .boldTitleFont
-        castLabel.textColor = .defaultColor
-        castLabel.text = "Cast"
+        detailView.overviewLabel.text = data?.overview
     }
 
 }
 
 extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        list.count
+        detailView.list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CastTableViewCell.identifier, for: indexPath) as! CastTableViewCell
-        let data = list[indexPath.row]
+        let data = detailView.list[indexPath.row]
         cell.configure(data: data)
         
         return cell
-    }
-}
-
-extension DetailViewController {
-    func getMovieCredit() {
-        NetworkManager.shared.getMovieCredit(id: data!.id) { result in
-            switch result {
-            case .success(let success):
-                self.list = success.cast
-            case .failure(let failure):
-                self.view.makeToast("크레딧 정보를 받아오지 못했습니다. \(failure.localizedDescription)", duration: 2, position: .bottom)
-            }
-        }
     }
 }
---
+++ Media/View/DetailView.swift
@@ -0,0 +1,114 @@
+//
+//  DetailView.swift
+//  Media
+//
+//  Created by 조규연 on 6/22/24.
+//
+
+import UIKit
+import SnapKit
+import Toast
+
+class DetailView: UIView {
+    let posterImageView = UIImageView()
+    let infoView = UIView()
+    let titleLabel = UILabel()
+    let overviewLabel = UILabel()
+    let castLabel = UILabel()
+    let castTableView = UITableView()
+
+    var list: [Cast] = [] {
+        didSet {
+            castTableView.reloadData()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierachy()
+        configureLayout()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension DetailView {
+    func configureHierachy() {
+        addSubviews([posterImageView, infoView])
+        infoView.addSubviews([titleLabel, overviewLabel, castLabel, castTableView])
+    }
+    
+    func configureLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        infoView.snp.makeConstraints {
+            $0.top.equalTo(self.snp.centerY)
+            $0.horizontalEdges.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(30)
+            $0.top.equalToSuperview().offset(20)
+        }
+        
+        overviewLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(30)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+        }
+        
+        castLabel.snp.makeConstraints {
+            $0.top.equalTo(overviewLabel.snp.bottom).offset(20)
+            $0.leading.equalTo(overviewLabel)
+        }
+        
+        castTableView.snp.makeConstraints {
+            $0.top.equalTo(castLabel.snp.bottom).offset(8)
+            $0.horizontalEdges.equalTo(overviewLabel)
+            $0.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    func configureUI() {
+        castTableView.register(CastTableViewCell.self, forCellReuseIdentifier: CastTableViewCell.identifier)
+        castTableView.rowHeight = 80
+        
+        posterImageView.contentMode = .scaleAspectFill
+        
+        infoView.backgroundColor = .white
+        infoView.clipsToBounds = true
+        infoView.layer.cornerRadius = 20
+        infoView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        titleLabel.font = .bigTitleFont
+        titleLabel.textColor = .defaultColor
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        
+        overviewLabel.font = .contentFont
+        overviewLabel.textColor = .defaultColor
+        overviewLabel.numberOfLines = 0
+        
+        castLabel.font = .boldTitleFont
+        castLabel.textColor = .defaultColor
+        castLabel.text = "Cast"
+    }
+}
+
+extension DetailView {
+    func getMovieCredit(data: movieResult) {
+        NetworkManager.shared.getMovieCredit(id: data.id) { result in
+            switch result {
+            case .success(let success):
+                self.list = success.cast
+            case .failure(let failure):
+                self.makeToast("크레딧 정보를 받아오지 못했습니다. \(failure.localizedDescription)", duration: 2, position: .bottom)
+            }
+        }
+    }
+}
